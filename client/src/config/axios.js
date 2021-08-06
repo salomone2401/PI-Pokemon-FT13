e8f90acde55ab7975478b582d2f01ed97fbc66a6
ana paula <salomoneanapaulah3@gmail.com>
2021-08-06 12:29:19 -0300

    new commit

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL : 'https://quiet-beyond-42912.herokuapp.com'
+    baseURL : 'https://quiet-beyond-42912.herokuapp.com/'
 });
 
 export default clientAxios;
