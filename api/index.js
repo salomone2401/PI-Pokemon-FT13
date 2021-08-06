ddf9dd8fe4d93542a99859301ff82755a171bcc3
ana paula <salomoneanapaulah3@gmail.com>
2021-08-06 12:08:37 -0300

    deploy fix

--- api/index.js
+++ api/index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const port = process.env.PORT || 4000;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, '0.0.0.0',() => {
+  server.listen(port, '0.0.0.0',() => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
